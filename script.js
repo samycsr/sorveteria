5db8123e58ca7ebbc7d780ea1b8fec5cfb8ab990
samycsr <137799042+samycsr@users.noreply.github.com>
2023-09-19 09:11:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+botao.addEventListener('click',function(){
+
+
+var nomeCliente = nome. value;
+var quantiaSorvete = quantidade. value;
+var saborSorvete = sabores. value;
+
+
+//alert("nome:" + nomeCliente + "Qnt:" + quantiaSorvete + "sabor" + saborSorvete);
+
+
+var pedidoCliente = document.getElementById("peido")
+var dadosPedidos = "nome:" + nomeCliente + "qnt:" + quantidaDeSorvete + "sabor:" + saborSorvete
+
+pedidoCliente. innerHTML = dadosPedido;
+pedidoCliente. innerHTML = quantiaSorvete;
+pedidoCliente. innerHTML = saborSorvete;
+
+
+
+});
+
+
+
+
